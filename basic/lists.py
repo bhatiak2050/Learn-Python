290d42b5f1fee13d902dde1a23781405436544de
Karan Bhatia <karanbhatia2050@gmail.com>
2019-06-07 09:36:05 +0530

    Update lists.py

--- basic/lists.py
+++ basic/lists.py
@@ -2,3 +2,11 @@ mylist = [1, 2, 3, 4.1, 5.99, 'hello']
 mylist.append('world')
 for x in mylist:
     print(x)
+
+munbers = []
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+print(numbers[0])
+print(numbers[1])
+print(numbers[2])
