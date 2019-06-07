1f79a09bfa7f707bf69a020f7abf218a44cdd98d
Karan Bhatia <karanbhatia2050@gmail.com>
2019-06-07 17:54:13 +0530

    Create range.py

---
+++ basic/range.py
@@ -0,0 +1,10 @@
+for i in range(10):
+        print(i, end=" ") # print numbers from 0 to 9
+
+print();                  # prints newline
+for i in range(5,10):
+        print(i, end=" ") # print numbers from 5 to 10
+
+print()                   # prints newline
+for i in range(0,11,2):
+        print(i, end=" ") # print even numbers from 1 to 10
