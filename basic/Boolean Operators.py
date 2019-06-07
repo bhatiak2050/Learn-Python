618d77c3bbd5f4bb5f85dadf9b5ce3c466b09e60
Karan Bhatia <karanbhatia2050@gmail.com>
2019-06-07 11:33:26 +0530

    Create Boolean Operators.py

---
+++ basic/Boolean Operators.py
@@ -0,0 +1,6 @@
+name = "Sam"
+age = 25
+if name == "Sam" and age == 25:
+        print("You are Sam and your age is 25")
+elif name == "Rick" or name == "Sam":
+        print("You are either Rick or a different Sam")
