92a1fda34501a9b4654755910d46b8656d465412
Karan Bhatia <karanbhatia2050@gmail.com>
2019-06-09 17:08:47 +0530

    Create variations_of_int().py

---
+++ basic/variations_of_int().py
@@ -0,0 +1,8 @@
+s = "15"
+print("from Decimal: %d" % int(s))
+s = "1111"
+print("from Binary: %d" % int(s, 2))
+s = "17"
+print("from Octal: %d" % int(s, 8))
+s = "F"
+print("from Hexadecimal: %d" % int(s, 16))
