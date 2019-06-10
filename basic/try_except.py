2165ddc9544c3f9b50530d13ae1f98ddf1d38edc
Karan Bhatia <karanbhatia2050@gmail.com>
2019-06-10 14:37:04 +0530

    Create try_except.py

---
+++ basic/try_except.py
@@ -0,0 +1,8 @@
+prompt = "Enter your age: "
+try:
+        age = int(input(prompt))
+        print("Your age is ",str(age))
+except:
+        print("Please enter a number\n")
+
+print("Program successful")
