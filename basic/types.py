76ba6e13bbdd9b5efa83a338e8e1aecd42f8e5eb
Karan Bhatia <karanbhatia2050@gmail.com>
2019-06-11 09:23:23 +0530

    Create types.py
    List of a few data types supported by python.

---
+++ basic/types.py
@@ -0,0 +1,14 @@
+def printtype(x):
+        print(type(x))
+
+x = 5
+printtype(x)
+x = 12.56
+printtype(x)
+x = "Hello"
+printtype(x)
+x = True
+printtype(x)
+x = None
+printtype(x)
+print(type(printtype))
