58d0a40ae47217501f74778bc63c8fa40541a8f2
Karan Bhatia <karanbhatia2050@gmail.com>
2019-06-12 17:53:41 +0530

    Create Mergesort_strings.py

---
+++ Programs/Mergesort_strings.py
@@ -0,0 +1,50 @@
+def merge(left, nl, right, nr, a):
+	i=0;j=0;k=0
+
+	while i<nl and j<nr:
+		if(left[i] <= right[j]):
+			a[k] = left[i]
+			i+=1; k+=1
+		else:
+			a[k] = right[j]
+			j+=1; k+=1
+	while i<nl:
+		a[k] = left[i]
+		i+=1; k+=1
+	while j<nr:
+		a[k] = right[j]
+		j+=1; k+=1
+
+def mergesort(names):
+	n = len(names)
+	if n<2: return
+
+	mid = int(n/2)
+	left = []
+	right = []
+
+
+	for i in range(0, mid):
+		left.append(names[i])
+
+	for i in range(mid, n):
+		right.append(names[i])
+
+	print(left,right)
+
+	mergesort(left)
+	mergesort(right)
+	merge(left, mid, right, n-mid, names)
+	print(left,right)
+
+names = []
+name = input("Enter the names ('done' to stop)\n")
+while(name != 'done'):
+	names.append(name)
+	name = input()
+
+mergesort(names)
+
+print("\nThe names in alplabetical order are:")
+for name in names:
+	print(name)
