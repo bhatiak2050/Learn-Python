3900f30ad624b4205c83c22e56dd4e6d92770e9b
Karan Bhatia <karanbhatia2050@gmail.com>
2019-06-15 16:04:17 +0530

    Update list_methods.py

--- basic/list_methods.py
+++ basic/list_methods.py
@@ -5,3 +5,11 @@ print("Minimum:",min(list))
 print("Maximum:"max(list))
 print("Sum:"sum(list))
 print("Average:",sum(list)/len(list))
+
+string = "The Quick Brown Fox Jumps Over The Lazy Dog"
+list1 = list(string)
+print(list1)
+
+delimeter  = ' '
+string2 = delimeter.join(list1)
+print(string2)
