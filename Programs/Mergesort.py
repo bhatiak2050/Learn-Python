fab55f8da1acbbcc3b33fab2099ecde37fbd28f5
Karan Bhatia <karanbhatia2050@gmail.com>
2019-06-15 16:30:50 +0530

    Update Mergesort.py
    Shortened the code by using list slices.

--- Programs/Mergesort.py
+++ Programs/Mergesort.py
@@ -1,47 +1,36 @@
 def merge(left, nl, right, nr, a):
-	i=0;j=0;k=0
-	while i<nl and j<nr:
-		if(left[i] <= right[j]):
-			a[k] = left[i]
-			k+=1; i+=1
-		else:
-			a[k] = right[j]
-			j+=1; k+=1
-	while i<nl:
-		a[k] = left[i]
-		i+=1; k+=1
-	while j<nr:
-		a[k] = right[j]
-		j+=1; k+=1
+        i,j,k = 0,0,0
+        while i<nl and j<nr:
+                if(left[i] <= right[j]):
+                        a[k] = left[i]
+                        k,i = k+1,i+1
+                else:
+                        a[k] = right[j]
+                        k,j = k+1,j+1
+
+        if(i<nl): a[k:] = left[i:]
+        if(j<nr): a[k:] = right[j:]
 
 def mergesort(list, n):
-	if n<2: return
+        if n<2: return
 
-	mid = int(n/2)
-	left = []
-	right = []
+        mid = int(n/2)
+        left = list[0:mid]
+        right = list[mid:n]
 
-	for i in range(0,mid):
-		left.append(0)
-		left[i] = list[i]
-	for i in range(mid, n):
-		right.append(0)
-		right[i-mid] = list[i]
-
-	mergesort(left, mid)
-	mergesort(right, n-mid)
-	merge(left, mid, right, n-mid, list)
+        mergesort(left, mid)
+        mergesort(right, n-mid)
+        merge(left, mid, right, n-mid, list)
 
 list = []
-print("Enter the number of elements: ")
-n = int(input())
-print("Enter the elements: ")
-for i in range(0,n):
-	list.append(0)
-	list[i] = int(input())
+print("Enter the elements ('done' to break)")
+while(True):
+        inp = input()
+        if inp == 'done': break
+        elif not inp.isdigit(): continue
+        else: list.append(int(inp))
 
-mergesort(list, n)
+mergesort(list, len(list))
 
 print("\nThe sorted array is :")
-for i in range(0,n):
-	print(list[i], end=" ")
+print(list)
