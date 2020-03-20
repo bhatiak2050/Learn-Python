60d5f1b1147167e440447decabc91987ce9c15d9
Karan Bhatia <karanbhatia2050@gmail.com>
2020-03-20 06:23:33 +0000

    Update Towers_of_Hanoi.py

--- Programs/Towers_of_Hanoi.py
+++ Programs/Towers_of_Hanoi.py
@@ -1,9 +1,9 @@
 def toh(n, beg, aux, end):
 	if n==1:
-		print("Disk 1: %s -> %s\n" % (beg, end), end="")
+		print("Disk 1: %s -> %s" % (beg, end))
 		return
 	toh(n-1, beg,end,aux)
-	print("Disk %d: %s -> %s\n" % (n, beg, end), end="")
+	print("Disk %d: %s -> %s" % (n, beg, end))
 	toh(n-1, aux,beg,end)
 
 n = int(input("Enter the number of disks: "))
